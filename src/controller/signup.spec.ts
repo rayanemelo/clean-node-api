38610d013e2376d5468db718263f06c0f512dce5
rayanemelo <rayanemeloc@lighthouseit.com.br>
2022-08-10 21:57:22 -0300

    test email test

--- src/controller/signup.spec.ts
+++ src/controller/signup.spec.ts
@@ -2,9 +2,9 @@ import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
   test('Deve retornar 400, se o nome não for fornecido', () => {
-    const sut = new SignUpController();
+    const sut = new SignUpController()
     const httpRaquest = {
-      //post
+      // post
       body: {
         email: 'any_email@email.com',
         password: 'any_password',
@@ -13,5 +13,20 @@ describe('SignUp Controller', () => {
     }
     const httpResponde = sut.handle(httpRaquest)
     expect(httpResponde.statusCode).toBe(400)
+    expect(httpResponde.body).toEqual(new Error('Missign param: name'))
+  })
+
+  test('Deve retornar 400, se o email não for fornecido', () => {
+    const sut = new SignUpController()
+    const httpRaquest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponde = sut.handle(httpRaquest)
+    expect(httpResponde.statusCode).toBe(400)
+    expect(httpResponde.body).toEqual(new Error('Missign param: email'))
   })
 })

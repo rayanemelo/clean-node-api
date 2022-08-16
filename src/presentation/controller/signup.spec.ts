429513a015f28187d091620deae08df913c22361
rayanemelo <rayanemeloc@lighthouseit.com.br>
2022-08-16 20:18:49 -0300

    test: mock emailValidator

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,8 +1,27 @@
 import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-param-error'
+import { InvalidParamError } from '../errors/invalid-param-error'
+import { EmailValidator } from '../protocols/email-validator'
+
+interface SutType {
+  sut: SignUpController
+  emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutType => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new SignUpController(emailValidatorStub)
+  return { sut, emailValidatorStub }
+}
+
 describe('SignUp Controller', () => {
   test('Deve retornar 400, se o nome não for fornecido', () => {
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRaquest = {
       // post
       body: {
@@ -17,7 +36,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Deve retornar 400, se o email não for fornecido', () => {
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRaquest = {
       body: {
         name: 'any_name',
@@ -31,7 +50,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Deve retornar 400, se a senha não for fornecida', () => {
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRaquest = {
 
       body: {
@@ -46,7 +65,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Deve retornar 400, se a confirmação de senha não for fornecida', () => {
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRaquest = {
 
       body: {
@@ -58,5 +77,21 @@ describe('SignUp Controller', () => {
     const httpResponde = sut.handle(httpRaquest)
     expect(httpResponde.statusCode).toBe(400)
     expect(httpResponde.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
+
+  test('Deve retornar 400, se o email fornecido for inválido', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false) // Mocando o validator
+    const httpRaquest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponde = sut.handle(httpRaquest)
+    expect(httpResponde.statusCode).toBe(400)
+    expect(httpResponde.body).toEqual(new InvalidParamError('email'))
   })
 })
